cdd07bef9d5e90318acfd3ec5d00c84cadbe1f89
c0dz@r20 <77671710+c0dz2r20@users.noreply.github.com>
2021-01-27 17:56:28 +0530

    Add files via upload

---
+++ scripts/admin_leaves.js
@@ -0,0 +1,48 @@
+var firebaseConfig = {
+   apiKey: "AIzaSyALFeMcSa3Sw3nigGK4JRyJSE3LKPvyeRw",
+   authDomain: "adminpanel-da9d9.firebaseapp.com",
+   projectId: "adminpanel-da9d9",
+   storageBucket: "adminpanel-da9d9.appspot.com",
+   messagingSenderId: "130078402633",
+   appId: "1:130078402633:web:7e1cf2b82b90f9a7e17798",
+   measurementId: "G-382QL62H9W"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore()
+db_leaves = db.collection('leave_requests')
+
+leave_details_db = () => {
+   let member_leaves_table_body = document.getElementsByClassName('admin_leave_table_body')[0]
+   db_leaves.get().then((querySnapshot) => {
+       querySnapshot.forEach((doc) => {
+           let tr = document.createElement('tr')
+           
+           // Leave Request Details
+           let td_leave_details = document.createElement('td')
+           td_leave_details.innerText = doc.data().leaveDetails
+           td_leave_details.style.wordWrap = 'break-word'
+           td_leave_details.style.verticalAlign = "middle"
+           td_leave_details.style.width = '350px'
+           tr.appendChild(td_leave_details)
+           
+           //  Leave Peroid Details
+           let td_leave_period = document.createElement('td')
+           td_leave_period.style.verticalAlign = "middle"
+           td_leave_period.innerText = `${doc.data().leaveStartDate} :: ${doc.data().leaveEndDate}`
+           tr.appendChild(td_leave_period)
+            
+           // Revoke Request
+           let td_delete_icon = document.createElement('img')
+           td_delete_icon.setAttribute('src', './assets/delete.svg')
+           td_delete_icon.classList.add('admin_action_delete')
+           td_delete_icon.style.verticalAlign = "middle"
+           tr.appendChild(td_delete_icon)
+           
+           member_leaves_table_body.appendChild(tr)
+       })
+   })
+}
+
+leave_details_db()
--- scripts/member_leaves.js
+++ scripts/member_leaves.js
@@ -33,15 +33,56 @@ raise_request.onclick = () => {
         leave_details.value = ''
         leave_start_date.value = ''
         leave_end_date.value = ''
+        
     }
 }
 
+db_leaves = db.collection('leave_requests')
 leave_request_toDb = () => {
-    db_leave_request = db.collection('leave_requests').doc()
-    db_leave_request.set({
+    db_leaves.doc().set({
         leaveDetails: leave_details.value,
         leaveStartDate: leave_start_date.value,
         leaveEndDate: leave_end_date.value,
         leaveStatus: 'Pending'
     })
 }
+
+leave_details_db = () => {
+    let member_leaves_table_body = document.getElementsByClassName('member_leaves_table_body')[0]
+    db_leaves.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            let tr = document.createElement('tr')
+            
+            // Leave Request Details
+            let td_leave_details = document.createElement('td')
+            td_leave_details.innerText = doc.data().leaveDetails
+            td_leave_details.style.wordWrap = 'break-word'
+            td_leave_details.style.verticalAlign = "middle"
+            td_leave_details.style.width = '350px'
+            tr.appendChild(td_leave_details)
+            
+           //  Leave Peroid Details
+           let td_leave_period = document.createElement('td')
+           td_leave_period.style.verticalAlign = "middle"
+           td_leave_period.innerText = `${doc.data().leaveStartDate} :: ${doc.data().leaveEndDate}`
+           tr.appendChild(td_leave_period)
+            
+            // Leave Status
+            let td_leave_status = document.createElement("td")
+            td_leave_status.style.verticalAlign = "middle"
+            td_leave_status.innerText = doc.data().leaveStatus
+            tr.appendChild(td_leave_status)
+            
+            // Revoke Request
+            let td_delete_icon = document.createElement('img')
+            td_delete_icon.setAttribute('src', './assets/delete.svg')
+            td_delete_icon.classList.add('admin_action_delete')
+            td_delete_icon.style.verticalAlign = "middle"
+            tr.appendChild(td_delete_icon)
+            
+            member_leaves_table_body.appendChild(tr)
+        })
+    })
+}
+
+leave_details_db()

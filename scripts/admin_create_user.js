5c9dd611d1527d431af83c953f2a05a905d4e1eb
c0dz@r20 <77671710+c0dz2r20@users.noreply.github.com>
2021-02-02 15:49:08 +0530

    Update admin_create_user.js

--- scripts/admin_create_user.js
+++ scripts/admin_create_user.js
@@ -9,9 +9,8 @@ var firebaseConfig = {
  };
  // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
-
- 
 let db = firebase.firestore();
+let auth = firebase.auth()
 
 // Referencing fields here
 let admin_create_user_userType = document.getElementById('admin-create-user-userType'),
@@ -39,104 +38,118 @@ admin_create_user_btn.onclick = (e) => {
    else if (admin_create_user_email.value === '') {
       alert ('Email is not valid !!!')
    }
-   else if (admin_create_user_mobileNumber === '') {
+   else if (admin_create_user_mobileNumber.value === '' || admin_create_user_mobileNumber.value.length != 10) {
       alert ('Mobile Number is not valid !!!')
    }
    else {
-      let admin_db = db.collection('new_users').doc()
-      admin_db.set({
-         userType: admin_create_user_userType.value,
-         firstName: admin_create_user_firstName.value,
-         lastName: admin_create_user_lastName.value,
-         pass: 'Admin123$',
-         email: admin_create_user_email.value,
-         mobileNumber: admin_create_user_mobileNumber.value,
-         timestamp: firebase.firestore.FieldValue.serverTimestamp()
-      })
-      alert('User created successfully !!!')
-      addedUsers()
-      admin_create_user_userType.value = 'User Type'
-      admin_create_user_firstName.value = ''
-      admin_create_user_lastName.value = ''
-      admin_create_user_email.value = ''
-      admin_create_user_mobileNumber.value = ''
-
       
-      showCreatedUser()
+      // Adding user to firebase authentication.
+      addedUsers()
+      let admin_db = db.collection('new_users').doc()
+         admin_db.set({
+            userType: admin_create_user_userType.value,
+            firstName: admin_create_user_firstName.value,
+            lastName: admin_create_user_lastName.value,
+            pass: 'Admin123$',
+            email: admin_create_user_email.value,
+            mobileNumber: admin_create_user_mobileNumber.value,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+         })
+         alert('User created successfully !!!')
    }
 }
 
 addedUsers = () => {
    let authUser = admin_create_user_email.value;
-   console.log(authUser);
    let authPass = 'Admin123$'
-   let displayName = admin_create_user_firstName.value + admin_create_user_lastName.value
-   firebase.auth().createUserWithEmailAndPassword(authUser, authPass).then((userCredential) => {
-      console.log(userCredential.user);
-      
-   }).catch(error => {console.log(error.message);})
+   auth.createUserWithEmailAndPassword(authUser, authPass)
+   .catch(error => {console.log(error);})
+   .then((cred) => {
+      if (cred.user != null) {
+         let displayName = admin_create_user_firstName.value + " " + admin_create_user_lastName.value
+        auth.currentUser.updateProfile({
+            displayName: displayName,
+            phoneNumber : admin_create_user_mobileNumber.value
+         })
+         cred.user.sendEmailVerification()
+         // Clearing values on submit
+         admin_create_user_userType.value = 'User Type'
+         admin_create_user_firstName.value = ''
+         admin_create_user_lastName.value = ''
+         admin_create_user_email.value = ''
+         admin_create_user_mobileNumber.value = ''
+      }
+
+      else {
+         console.log('Unable to create users.');
+      }
+   })
+   
 }
-
-
 
 // Showing Created users in the table
 showCreatedUser = () => {
    let show_admin_data = db.collection('new_users')
-   let admin_remove_old_data =  document.querySelectorAll('.admin-table-class-name')
-   for(delRows of admin_remove_old_data) {
-      delRows.remove()
-   }
-   show_admin_data.get().then(function(querySnapshot) {
+   
+   show_admin_data.orderBy('timestamp', 'desc').onSnapshot(querySnapshot => {
+      let admin_remove_old_data =  document.querySelectorAll('.admin-table-class-name')
+      for(delRows of admin_remove_old_data) {
+         delRows.remove()
+      }
       querySnapshot.forEach(function(doc) {
-         let body_row = document.createElement('tr')
-         body_row.classList.add('admin-table-class-name')
-
-         // Table Data for First Name and Last Name
-         let td_name = document.createElement('td')
-         td_name.innerText = doc.data().firstName + ' ' + doc.data().lastName
-         body_row.appendChild(td_name)
-
-         // Table Data for User Type
-         let td_userType = document.createElement('td')
-         td_userType.innerText = doc.data().userType
-         body_row.appendChild(td_userType)
-
-         // Table Data for Email
-         let td_email = document.createElement('td')
-         td_email.innerText = doc.data().email
-         body_row.appendChild(td_email)
-
-         // Table Data for Mobile Number
-         let td_mobileNumber = document.createElement('td')
-         td_mobileNumber.innerText = doc.data().mobileNumber
-         body_row.appendChild(td_mobileNumber)
-
-         // Table Data for Created On
-         let td_logTime = document.createElement('td')
-         let outT = doc.data().timestamp.seconds
-         let n = new Date()
-         n.setTime(outT * 1000)
-         td_logTime.innerText = n
-         body_row.appendChild(td_logTime)
-
-         // Table Data for Delete/Edit
-         let td_delete_edit = document.createElement('td')
-         let td_delete_icon = document.createElement('img')
-         td_delete_icon.setAttribute('src', './assets/delete.svg')
-         td_delete_icon.classList.add('admin_action_delete')
-         td_delete_edit.appendChild(td_delete_icon)
-
-         let td_edit_icon = document.createElement('img')
-         td_edit_icon.setAttribute('src', './assets/edit.svg')
-         td_edit_icon.classList.add('admin_action_edit')
-         td_delete_edit.appendChild(td_edit_icon)
-
-         body_row.appendChild(td_delete_edit)
-
-         // Appending data to row
-         admin_show_user_table_body.appendChild(body_row)
-         
+         creatingTable(doc)
       })
    })
 }
 showCreatedUser()
+
+creatingTable = doc => {
+   let body_row = document.createElement('tr')
+   body_row.classList.add('admin-table-class-name')
+
+   // Table Data for First Name and Last Name
+   let td_name = document.createElement('td')
+   td_name.innerText = doc.data().firstName + ' ' + doc.data().lastName
+   body_row.appendChild(td_name)
+
+   // Table Data for User Type
+   let td_userType = document.createElement('td')
+   td_userType.innerText = doc.data().userType
+   body_row.appendChild(td_userType)
+
+   // Table Data for Email
+   let td_email = document.createElement('td')
+   td_email.innerText = doc.data().email
+   body_row.appendChild(td_email)
+
+   // Table Data for Mobile Number
+   let td_mobileNumber = document.createElement('td')
+   td_mobileNumber.innerText = doc.data().mobileNumber
+   body_row.appendChild(td_mobileNumber)
+
+   // Table Data for Created On
+   let td_logTime = document.createElement('td')
+   let outT = doc.data().timestamp.seconds
+   console.log(outT);
+   let n = new Date()
+   n.setTime(outT * 1000)
+   td_logTime.innerText = n
+   body_row.appendChild(td_logTime)
+
+   // Table Data for Delete/Edit
+   let td_delete_edit = document.createElement('td')
+   let td_delete_icon = document.createElement('img')
+   td_delete_icon.setAttribute('src', './assets/delete.svg')
+   td_delete_icon.classList.add('admin_action_delete')
+   td_delete_edit.appendChild(td_delete_icon)
+
+   let td_edit_icon = document.createElement('img')
+   td_edit_icon.setAttribute('src', './assets/edit.svg')
+   td_edit_icon.classList.add('admin_action_edit')
+   td_delete_edit.appendChild(td_edit_icon)
+
+   body_row.appendChild(td_delete_edit)
+
+   // Appending data to row
+   admin_show_user_table_body.appendChild(body_row)
+}

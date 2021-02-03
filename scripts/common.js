6bf8d37fdd899904e1028291c59c1d4a971da1bd
c0dz@r20 <77671710+c0dz2r20@users.noreply.github.com>
2021-02-03 10:55:41 +0530

    Common script

---
+++ scripts/common.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyALFeMcSa3Sw3nigGK4JRyJSE3LKPvyeRw",
+    authDomain: "adminpanel-da9d9.firebaseapp.com",
+    projectId: "adminpanel-da9d9",
+    storageBucket: "adminpanel-da9d9.appspot.com",
+    messagingSenderId: "130078402633",
+    appId: "1:130078402633:web:7e1cf2b82b90f9a7e17798",
+    measurementId: "G-382QL62H9W"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  let db = firebase.firestore(),
+  auth = firebase.auth()
